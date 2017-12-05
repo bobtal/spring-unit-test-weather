a2eb5b7080e9c02e5c28efcab4f9cde80aa3383e
Boban Talevski <talevski.boban@gmail.com>
2017-12-05 08:30:07 +0100

    Added comments for MockMvc and MockMvcResultMatchers in WeatherControllerTest.java

--- src/test/java/com/teamtreehouse/web/controller/WeatherControllerTest.java
+++ src/test/java/com/teamtreehouse/web/controller/WeatherControllerTest.java
@@ -9,6 +9,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//What you "expect" after performing a request on a MockMvc object is called a ResultMatcher.
+// Typically your options will come from the MockMvcResultMatchers class. Some things
+// you can examine are the following:
+//  content(): check the response body for actual content in the HTTP response
+//  flash(): check for certain flash attributes
+//  model(): check for model attributes
+//  jsonPath(...): check for content in an HTTP response body that is in the JSON format (e.g. for a REST API)
+// For a full list of matchers, see the Spring docs for
+// org.springframework.test.web.servlet.result.MockMvcResultMatchers
+// https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/test/web/servlet/result/MockMvcResultMatchers.html
+
 public class WeatherControllerTest {
     private MockMvc mockMvc;
     private WeatherController controller;
