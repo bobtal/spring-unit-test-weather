e38784443c3cf6664e9c46fb75c72d9114e3f103
Boban Talevski <talevski.boban@gmail.com>
2017-12-07 12:24:45 +0100

    End of Testing With Our Test Context section

--- src/test/java/com/teamtreehouse/service/WeatherServiceTest.java
+++ src/test/java/com/teamtreehouse/service/WeatherServiceTest.java
@@ -1,7 +1,11 @@
 package com.teamtreehouse.service;
 
 import com.teamtreehouse.config.AppConfig;
+import com.teamtreehouse.service.dto.geocoding.Location;
+import com.teamtreehouse.service.dto.weather.Weather;
 import com.teamtreehouse.service.resttemplate.weather.WeatherServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +16,57 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.hasSize;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class WeatherServiceTest {
+    @Autowired
+    private WeatherService service;
+
+    private Location loc;
+    private Weather weather;
+
+    private static final double ERROR_GEO = 0.0000001;
+    private static final double ERROR_TIME = 5000;
+
+    @Before
+    public void setUp() throws Exception {
+        loc = new Location(41.9403795, -87.65318049999999);
+        weather = service.findByLocation(loc);
+    }
+
+    // The purpose of these tests isn't to ensure that the weather API works
+    // (that's a responsibility of the API developers)
+    // but to ensure that our application properly captures those results
+    // and sticks the results into proper objects
+
+    // Test that lat and long that came from the API response
+    // match what we included in the request
+    @Test
+    public void findByLocation_ShouldReturnSameCoords() throws Exception {
+        assertThat(weather.getLatitude(), closeTo(loc.getLatitude(), ERROR_GEO));
+        assertThat(weather.getLongitude(), closeTo(loc.getLongitude(), ERROR_GEO));
+    }
+
+    @Test
+    public void findByLocation_ShouldReturn8DaysForecastData() throws Exception {
+        assertThat(weather.getDaily().getData(), hasSize(8));
+    }
+
+    // Test that we are getting back the current conditions
+    @Test
+    public void findByLocation_ShouldReturnCurrentConditions() throws Exception {
+        Instant now = Instant.now();
+        double duration = Duration.between(now, weather.getCurrently().getTime()).toMillis();
+        assertThat(duration, closeTo(0, ERROR_TIME));
+    }
+
 
     @Configuration
     @PropertySource("api.properties")
